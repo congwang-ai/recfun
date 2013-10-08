ac4d6e86a887e1cc335bd18e5f5f4f792406fdb0
CONG Wang <joshua.cong@gmail.com>
2013-10-08 17:24:08 -0400

    *

---
+++ target/scala-2.10/progfun-recfun_2.10-1.0.0/recfun/Main.scala
@@ -0,0 +1,41 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+    
+    //println(countChange(4,List(1,2)))
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int =
+    if (c == r) 1 else if (c == 0) 1 else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+
+    def aux(x: List[Char], count: Int): Boolean =
+      if (x.isEmpty && count == 0) true else if (count < 0) false else if (x.head == '(') aux(x.tail, count + 1) else if (x.head == ')') aux(x.tail, count - 1) else
+        aux(x.tail, count)
+
+    aux(chars, 0)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (money == 0) 1 else if (money < 0) 0 else if (coins.isEmpty) 0 else
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+}
